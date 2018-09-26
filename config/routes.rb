9d6be3ce483fad39161571b55f01fba2100cc937
Camilo Lopez <camilo.lopez@codescrum.com>
2018-09-26 17:15:59 -0500

    Add routes for the rate api controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  namespace :api, defaults: {format: 'json'} do
+    namespace :v1 do
+      scope :rates do
+        get '/' => 'rates#index', as: :rates
+        get '/:id' => 'rates#show'
+        post '/' => 'rates#create'
+        patch '/:id' => 'rates#update'
+        put '/:id' => 'rates#update'
+        delete '/:id' => 'rates#destroy'
+      end
+    end
+  end
+
+
+
 end
