6219da60773d981dc1fc25d87c9efa463dbd989b
Camilo Lopez <camilo.lopez@codescrum.com>
2018-09-28 14:15:12 -0500

    Add the route for show of the new insurances_controller (no api)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  scope :insurances do
+    get '/:id' => 'insurances#show'
+  end
 
   namespace :api, defaults: {format: 'json'} do
     namespace :v1 do
